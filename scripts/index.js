13606e51eea1cd57f26908b2f7e36981c55299f3
Maryna S <rimana.ca@gmail.com>
2026-08-21 21:55:51 -0400

    fix: files update

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-//https://rolling-scopes-school.github.io/m4ryn4-JSFEPRESCHOOL2023Q2/random-game/
-
 const cells = document.querySelectorAll(".cell");
 const txtCont = document.querySelector("#text-content");
 const newGameBtn = document.querySelector("#btn-new-game");
@@ -16,133 +14,119 @@ const combinations = [
 let cellsOptions = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
 let gamePlay = false;
-
 let score = [];
 const nextRoundBtn = document.querySelector("#btn-next-round");
 const gameScore = document.querySelector("#games-score");
+let wonGame;
 //1
 initializeGame();
 //1.2
 function initializeGame(){
-    cells.forEach(cell => cell.addEventListener("click", cellClicked));//1.3
-    newGameBtn.addEventListener("click", restartGame);//1.4
-    let fight = new Audio('audio/fight.mp3');
-    fight.play();
+  cells.forEach(cell => cell.addEventListener("click", cellClicked));//1.3
+  newGameBtn.addEventListener("click", restartGame);//1.4
+    // let fight = new Audio('audio/fight.mp3');
+    // fight.play();
+  nextRoundBtn.addEventListener("click", nextRoundGame);
 
-    nextRoundBtn.addEventListener("click", nextRoundGame);
-
-    txtCont.textContent = `${currentPlayer}'s turn`;
-    gamePlay = true;
+  txtCont.textContent = `${currentPlayer}'s turn`;
+  gamePlay = true;
 }
 //2
-function cellClicked(){
-    const idx = this.getAttribute("idx");//If does not exist, value = ""
+function cellClicked(){    
+  const dataIndex = this.getAttribute("data-index");//If does not exist, value = ""
 
-    if(cellsOptions[idx] != "" || gamePlay == false){//!!disallow to click TWICE if cell has something & game is not finished
-        return;
-    }
+  if (cellsOptions[dataIndex] != "" || gamePlay == false){//!!disallow to click TWICE if cell has something & game is not finished
+    return;
+  }
 
-    updateCell(this, idx);//3
-    checkWinner();//4
+  updateCell(this, dataIndex);//3
+  checkWinner();//4
 }
 //3
 function updateCell(cell, index){
-    cellsOptions[index] = currentPlayer;//upd backend
-    cell.textContent = currentPlayer;//upd front
+  cellsOptions[index] = currentPlayer;//upd backend
+  cell.textContent = currentPlayer;//upd front
 }
 //4
 function changePlayer(){
-    currentPlayer = (currentPlayer == "X") ? "O" : "X";
-    txtCont.textContent = `${currentPlayer}'s turn`;
+  currentPlayer = (currentPlayer == "X") ? "O" : "X";
+  txtCont.textContent = `${currentPlayer}'s turn`;
 }
 //5
 function checkWinner(){
-    let wonGame = false;
+  wonGame = false;
 
-    for(let i = 0; i < combinations.length; i++){
-        if(cellsOptions[combinations[i][0]] == "" || cellsOptions[combinations[i][1]] == "" || cellsOptions[combinations[i][2]] == ""){
-            continue;
-        }
-        if(cellsOptions[combinations[i][0]] == cellsOptions[combinations[i][1]] && cellsOptions[combinations[i][1]] == cellsOptions[combinations[i][2]]){
-            const cells = document.getElementsByClassName('cell');
-
-            let fatality = new Audio('audio/fatality.mp3');
-            fatality.play();
-
-            setTimeout(() => {
-            cells[combinations[i][0]].classList.add('win-show');
-            cells[combinations[i][1]].classList.add('win-show');
-            cells[combinations[i][2]].classList.add('win-show');
-              }, 500);
-            wonGame = true;
-
-           break;
-        }
+  for (let i = 0; i < combinations.length; i++){
+    if(cellsOptions[combinations[i][0]] == "" || cellsOptions[combinations[i][1]] == "" || cellsOptions[combinations[i][2]] == ""){
+      continue;
     }
+    if(cellsOptions[combinations[i][0]] == cellsOptions[combinations[i][1]] && cellsOptions[combinations[i][1]] == cellsOptions[combinations[i][2]]){
+      const cells = document.getElementsByClassName('cell');
 
+      let fatality = new Audio('audio/fatality.mp3');
+      fatality.play();
+
+      setTimeout(() => {
+        cells[combinations[i][0]].classList.add('win-show');
+        cells[combinations[i][1]].classList.add('win-show');
+        cells[combinations[i][2]].classList.add('win-show');
+      }, 500);
+      wonGame = true;
+
+      break;
+      }
+    }
     if(wonGame){
-        txtCont.textContent = `${currentPlayer} wins!`;
-        gamePlay = false;
+      txtCont.textContent = `${currentPlayer} wins!`;
+      gamePlay = false;
 
-        // score.push(`${currentPlayer}`);
-        // gameScore.textContent = "";
-        // for(let i = 0; i < score.length; i++){
-        //     gameScore.textContent += `Round ${i+1} : ${score[i]} `;
-        // }
-
-        score.push(`${currentPlayer}`);
-        displayScore();
+      score.push(`${currentPlayer}`);
+      displayScore();
     } else if(!cellsOptions.includes("")){
     // } else if(!(cellsOptions[combinations[i][0]] == cellsOptions[combinations[i][1]] && cellsOptions[combinations[i][1]] == cellsOptions[combinations[i][2]])){
-        txtCont.textContent = `Draw!`;
-        gamePlay = false;
+      txtCont.textContent = `Draw!`;
+      gamePlay = false;
 
-        score.push(`Draw`);
-        displayScore();
+      score.push(`Draw`);
+      displayScore();
     } else{
-        changePlayer();
+      changePlayer();
     }
 }
 
 function displayScore(){
-    gameScore.textContent = "";
-    gameScore.setAttribute('style', 'white-space: pre;');
-    for(let i = 0; i < score.length; i++){
-        if(score[i] == "X" || score[i] == "O"){
-            gameScore.textContent += `Round ${i+1}: ${score[i]} won \r\n`;
-        } else {
-        gameScore.textContent += `Round ${i+1}: ${score[i]} \r\n`;
-        }
+  gameScore.textContent = "";
+  gameScore.setAttribute('style', 'white-space: pre;');
+  for(let i = 0; i < score.length; i++){
+    if(score[i] == "X" || score[i] == "O"){
+      gameScore.textContent += `Round ${i+1}: ${score[i]} won \r\n`;
+    } else {
+      gameScore.textContent += `Round ${i+1}: ${score[i]} \r\n`;
     }
+}
 }
 //6.
 function restartGame(){
-    currentPlayer = "X";
-    cellsOptions = ["", "", "", "", "", "", "", "", ""];
-    txtCont.textContent = `${currentPlayer}'s turn`;
-    cells.forEach(cell => cell.textContent = "");
-    cells.forEach(cell => cell.classList.remove("win-show"));
-    gamePlay = true;
+  currentPlayer = "X";
+  cellsOptions = ["", "", "", "", "", "", "", "", ""];
+  txtCont.textContent = `${currentPlayer}'s turn`;
+  cells.forEach(cell => cell.textContent = "");
+  cells.forEach(cell => cell.classList.remove("win-show"));
+  gamePlay = true;
 
-    location.reload();
+  location.reload();
 }
 
 function nextRoundGame(){
-    //for(let i = 0; i < score.length; i++){
-    //    alert(score[i]);
-    //}
-    let fight = new Audio('audio/fight.mp3');
-    fight.play();
+    // let fight = new Audio('audio/fight.mp3');
+    // fight.play();
 
-    currentPlayer = "X";
-    cellsOptions = ["", "", "", "", "", "", "", "", ""];
-    txtCont.textContent = `${currentPlayer}'s turn`;
-    cells.forEach(cell => cell.textContent = "");
-    cells.forEach(cell => cell.classList.remove("win-show"));
-    gamePlay = true;
+  currentPlayer = "X";
+  cellsOptions = ["", "", "", "", "", "", "", "", ""];
+  txtCont.textContent = `${currentPlayer}'s turn`;
+  cells.forEach(cell => cell.textContent = "");
+  cells.forEach(cell => cell.classList.remove("win-show"));
+  gamePlay = true;
 
-    wonGame++;
-   // txtCont.textContent++;
-   // gameScore.textContent++;
-   
+  wonGame++;   
 }
